3f1bb28e3d993ee5f668b80c3d4cd4f0c3bbd7eb
Vinicius Mayrink <vmayrink12@gmail.com>
2024-12-28 16:58:45 -0300

    feat: adds file spec functionality and overrides older flags

--- cmd/fakeapi/main.go
+++ cmd/fakeapi/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 type cliArgs struct {
-	HTTPMethod   string
-	EndpointName string
+	FilePath string
 }
 
 type Response struct {
@@ -29,41 +31,84 @@ const (
 )
 
 type Endpoint struct {
-	Method   string
-	Response map[string]string
+	InputPath     string         `json:"path"`
+	InputMethod   string         `json:"method"`
+	OutputStatus  int            `json:"status"`
+	OutputContent map[string]any `json:"content"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request, args *cliArgs) {
-	if r.Method != args.HTTPMethod {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+type Endpoints []Endpoint
+
+func (e Endpoint) Requested(r *http.Request) bool {
+	return r.Method == e.InputMethod && r.URL.Path == e.InputPath
+}
+
+func (es Endpoints) Requested(r *http.Request) (Endpoint, error) {
+	for _, endpoint := range es {
+		if endpoint.Requested(r) {
+			return endpoint, nil
+		}
+	}
+	return Endpoint{}, errors.New("endpoint not found")
+}
+
+func handler(w http.ResponseWriter, r *http.Request, endpoints Endpoints) {
+	activeEndpoint, err := endpoints.Requested(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
 
-	if r.URL.Path != fmt.Sprintf("/%s", args.EndpointName) {
-		http.Error(w, "Not Found", http.StatusNotFound)
-		return
-	}
-
-	response := Response{Message: "Hello, World!"}
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+	json.NewEncoder(w).Encode(activeEndpoint.OutputContent)
 }
 
-func newCLIArgs() *cliArgs {
-	httpMethod := flag.String("method", "GET", "HTTP Method for the fake endpoint")
-	endpointName := flag.String("endpoint", "", "Fake endpoint's name")
-	flag.Parse()
-
-	return &cliArgs{
-		HTTPMethod:   *httpMethod,
-		EndpointName: *endpointName,
+func newCLIArgs() (*cliArgs, error) {
+	if len(os.Args) < 2 {
+		return nil, errors.New("Inform the needed parameters")
 	}
+
+	return &cliArgs{FilePath: os.Args[1]}, nil
+}
+
+func newEndpointsFromFile(filePath string) ([]Endpoint, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := os.Open(fmt.Sprintf("%s/%s", dir, filePath))
+	if err != nil {
+		return nil, err
+	}
+
+	byteValue, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var endpoints []Endpoint
+	err = json.Unmarshal(byteValue, &endpoints)
+	if err != nil {
+		return nil, err
+	}
+
+	return endpoints, nil
 }
 
 func main() {
-	cliArgs := newCLIArgs()
+	cliArgs, err := newCLIArgs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	endpoints, err := newEndpointsFromFile(cliArgs.FilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		handler(w, r, cliArgs)
+		handler(w, r, endpoints)
 	})
 	http.ListenAndServe(":8080", nil)
 }
