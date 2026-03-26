118542cfe56c69286613df5ec6198932d149397e
Vinicius Mayrink <vmayrink12@gmail.com>
2026-03-26 10:39:01 -0300

    chore(server): bumps version to `0.8.0`

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
           || (baseName == "go.sum");
       };
 
-      serverVersion = "0.7.0";
+      serverVersion = "0.8.0";
       server = pkgs.buildGoModule {
         name = "fakeapi-server";
         src = src;
